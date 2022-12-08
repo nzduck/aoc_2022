50b2f1434eb10e689203743a14808f36a962e843
Alan H <nzduck@gmail.com>
2022-12-09 08:29:13 +1300

    initial commit

---
+++ src/day_5.py
@@ -0,0 +1,21 @@
+def day_5_part_1_answer(all_data):
+   pass
+
+
+def day_5_part_2_answer(all_data):
+   pass
+
+
+def main():
+   
+   # open the data file
+   with open('.\data\data.txt', 'r') as f:
+      all_data = f.readlines()
+
+   day_5_part_1_answer(all_data)
+   day_5_part_2_answer(all_data)
+   
+
+
+if __name__ == '__main__':
+   main()
---
+++ src/test_day_5.py
@@ -0,0 +1,14 @@
+import unittest
+
+from day_5 import *
+
+class Tests(unittest.TestCase):
+
+   def test_1(self):
+      pass
+
+
+
+
+if __name__ == '__main__':
+   unittest.main()
