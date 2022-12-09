3a5bc916a717b8e37737daa842947aa7f7471aef
Alan H <nzduck@gmail.com>
2022-12-09 17:19:50 +1300

    initial commit

---
+++ src/day_6.py
@@ -0,0 +1,11 @@
+def main():
+   
+   # open the data file
+   with open('.\data\data.txt', 'r') as f:
+      all_data = f.readlines()
+
+   
+
+
+if __name__ == '__main__':
+   main()
---
+++ src/test_day_6.py
@@ -0,0 +1,12 @@
+import unittest
+
+from day_6 import *
+
+class Tests(unittest.TestCase):
+   pass
+
+
+
+
+if __name__ == '__main__':
+   unittest.main()
