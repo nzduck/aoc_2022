3c5d1fa6d037ddec68a98b29fcde1342c90641e8
Alan H <nzduck@gmail.com>
2022-12-09 19:57:40 +1300

    completed day 6 part 1

--- src/day_6.py
+++ src/day_6.py
@@ -1,10 +1,40 @@
+def find_marker_in_buffer(buff):
+   print(buff)
+   count = 0
+   seen = []
+
+   for c in buff:
+      count += 1
+
+      if c in seen:
+         remove_front_of_list(seen, c)
+
+      seen.append(c)
+         
+      if len(seen) == 4:
+         break;
+   
+   return count
+
+
+def remove_front_of_list(l1, item_to_remove_to):
+   place = l1.index(item_to_remove_to)
+   del l1[:place + 1]
+
+
+def day_6_part_1(data):
+   pos = find_marker_in_buffer(data)
+
+   print(f"Start-of-packet marker detected at position {pos}.")
+
+
 def main():
    
    # open the data file
    with open('.\data\data.txt', 'r') as f:
-      all_data = f.readlines()
+      all_data = f.readline()
 
-   
+   day_6_part_1(all_data)
 
 
 if __name__ == '__main__':
--- src/test_day_6.py
+++ src/test_day_6.py
@@ -3,9 +3,27 @@ import unittest
 from day_6 import *
 
 class Tests(unittest.TestCase):
-   pass
+   
+   def test_find_marker_in_buffer(self):
+      param_list = [("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)
+         , ("nppdvjthqldpwncqszvftbrmjlhg", 6)
+         , ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)
+         , ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+
+      for p1, p2 in param_list:
+            with self.subTest():
+               result = find_marker_in_buffer(p1)
+               self.assertEqual(result, p2, "Marker was not found in correct position.")
 
 
+   def test_remove_front_of_list(self):
+      l1 = ['1','2','3','4','5','6','7','8','9']
+      to_remove_to = '3'
+      expected_result = ['4','5','6','7','8','9']
+
+      remove_front_of_list(l1, to_remove_to)
+
+      self.assertEqual(l1, expected_result, "Lists are not the same.")
 
 
 if __name__ == '__main__':
