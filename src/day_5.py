6251b1fb7a04f86b216d4f80b23049047e86a133
Alan H <nzduck@gmail.com>
2022-12-09 14:28:41 +1300

    completed part 1 of day 5

--- src/day_5.py
+++ src/day_5.py
@@ -1,5 +1,60 @@
+def init_map():
+   map = {
+      '1': ['H','T','Z','D'],
+      '2': ['Q','R','W','T','G','C','S'],
+      '3': ['P','B','F','Q','N','R','C','H'],
+      '4': ['L','C','N','F','H','Z'],
+      '5': ['G','L','F','Q','S'],
+      '6': ['V','P','W','Z','B','R','C','S'],
+      '7': ['Z','F','J'],
+      '8': ['D','L','V','Z','R','H','Q'],
+      '9': ['B','H','G','N','F','Z','L','D']    
+   }
+
+   return map
+
+def move_elem(l1, l2, num):
+   for n in range(num):
+      l2.append(l1.pop())
+
+
+def split_command(c):
+   c1 = c.split(' ')
+   num = int(c1[1])
+   from_list = c1[3]
+   to_list = c1[5]
+
+   return tuple((num, from_list, to_list))
+
+
+def generate_code(map):
+   ret = ""
+   for k in map.keys():
+      v = map.get(k).pop()
+      ret += str(v)
+
+   return ret
+
+
 def day_5_part_1_answer(all_data):
-   pass
+
+   map = init_map()
+
+   count = 0
+   for l in all_data:
+      if l.startswith('move '):
+         count += 1
+
+         print(l)
+         t = split_command(l.strip()) 
+
+         print(map.get(t[1]), map.get(t[2]))
+         move_elem(map.get(t[1]), map.get(t[2]), t[0])
+         print(map.get(t[1]), map.get(t[2]))
+
+         print()
+
+   print(f"{count} records processed. Code is {generate_code(map)}")
 
 
 def day_5_part_2_answer(all_data):
--- src/test_day_5.py
+++ src/test_day_5.py
@@ -4,10 +4,38 @@ from day_5 import *
 
 class Tests(unittest.TestCase):
 
-   def test_1(self):
-      pass
+   def test_move_elem(self):
+      l1 = [1,2,3,4]
+      l2 = [6,7,8,9]
+      
+      move_elem(l1, l2, 2)
+
+      self.assertEqual(l1, [1,2], "From lists are not equal.")
+      self.assertEqual(l2, [6,7,8,9,4,3], "Destination lists are not equal.")
 
 
+   def test_split_command(self):
+      num = 0
+      from_list = ""
+      to_list = ""
+      c1 = "move 2 from 7 to 3"
+
+      t = split_command(c1)
+
+      self.assertEqual(t[0], 2, "Number to move is incorrect.")
+      self.assertEqual(t[1], "7", "Number to move is incorrect.")
+      self.assertEqual(t[2], "3", "Number to move is incorrect.")
+
+
+   def test_generate_code(self):
+      d = {
+         "1" : [1,2,3],
+         "2" : [4,5,6,7],
+         "3" : [8,9,0]
+      }
+      expected = "370"
+
+      self.assertEqual(generate_code(d), expected, "Generated code does not equal expected code.")
 
 
 if __name__ == '__main__':
