8332cef56fccf14d6e0fb059ce7cc2bfb257c6f0
Alan H <nzduck@gmail.com>
2022-12-12 14:45:27 +1300

    completed part 2 of day 6

--- src/day_6.py
+++ src/day_6.py
@@ -1,4 +1,4 @@
-def find_packet_marker_in_buffer(buff):
+def find_string_of_different_chars_in_buffer(buff, num):
    count = 0
    seen = []
 
@@ -10,10 +10,18 @@ def find_packet_marker_in_buffer(buff):
 
       seen.append(c)
          
-      if len(seen) == 4:
+      if len(seen) == num:
          break;
    
    return count
+
+
+def find_message_marker_in_buffer(buff):
+   return find_string_of_different_chars_in_buffer(buff, 14)
+
+
+def find_packet_marker_in_buffer(buff):
+   return find_string_of_different_chars_in_buffer(buff, 4)
 
 
 def remove_front_of_list(l1, item_to_remove_to):
@@ -27,6 +35,12 @@ def day_6_part_1(data):
    print(f"Start-of-packet marker detected at position {pos}.")
 
 
+def day_6_part_2(data):
+   pos = find_message_marker_in_buffer(data)
+
+   print(f"Start-of-message marker detected at position {pos}.")
+
+
 def main():
    
    # open the data file
@@ -34,6 +48,7 @@ def main():
       all_data = f.readline()
 
    day_6_part_1(all_data)
+   day_6_part_2(all_data)
 
 
 if __name__ == '__main__':
--- src/test_day_6.py
+++ src/test_day_6.py
@@ -3,7 +3,25 @@ import unittest
 from day_6 import *
 
 class Tests(unittest.TestCase):
+
+   def test_find_string_of_different_chars_in_buffer(self):
+      pass
+
+
    
+   def test_find_message_marker_in_buffer(self):
+      param_list = [("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)
+         , ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)
+         , ("nppdvjthqldpwncqszvftbrmjlhg", 23)
+         , ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)
+         , ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+
+      for p1, p2 in param_list:
+            with self.subTest():
+               result = find_message_marker_in_buffer(p1)
+               self.assertEqual(result, p2, "Message marker was not found in correct position.")
+
+
    def test_find_packet_marker_in_buffer(self):
       param_list = [("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)
          , ("nppdvjthqldpwncqszvftbrmjlhg", 6)
@@ -13,7 +31,7 @@ class Tests(unittest.TestCase):
       for p1, p2 in param_list:
             with self.subTest():
                result = find_packet_marker_in_buffer(p1)
-               self.assertEqual(result, p2, "Marker was not found in correct position.")
+               self.assertEqual(result, p2, "Packet marker was not found in correct position.")
 
 
    def test_remove_front_of_list(self):
